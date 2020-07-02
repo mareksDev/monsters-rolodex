ca8b07398132ebcc37ec8a48e17b425614d54766
Marek S <mareks@live.com>
2020-07-02 14:49:12 +0300

    Updates

---
+++ precache-manifest.0b05296ef6f4c0930949f05fc7013253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73791772086e9fd00cde7491fe607c2c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dcda985d65996596831c",
+    "url": "/monsters-rolodex/static/css/main.a0ce3958.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcda985d65996596831c",
+    "url": "/monsters-rolodex/static/js/main.6b7a3967.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
